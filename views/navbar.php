d31f85ebbd3de076c9591876c17dc520de408105
JuiceWrld12334124 <vgeance50@gmail.com>
2022-04-23 23:54:38 +0200

    navbar initial commit

---
+++ views/navbar.php
@@ -0,0 +1,41 @@
+
+
+<div class="navbar">
+    <div class="container flex">
+        <h1 class="logo"><a href="./index.php">Myves</a></h1>
+        <nav>
+            <div>
+                <ul >
+                    <li >
+                        <a  <?php if (basename($_SERVER['SCRIPT_NAME'] == 'index.php')) { echo 'active';} ?>
+                            href=".">Home</a>
+                    </li>
+                
+                <?php if (!$loggedIn): ?>
+                <li><a href='login.php'>Login</a></li>
+                <?php endif; ?>
+                <?php
+                    if ($loggedIn):
+                    $getUser = $pdo->prepare('SELECT nickname FROM users WHERE id=:ID');
+                    $getUser->bindParam(':ID', $_SESSION['userid']);
+                    if (!$getUser->execute()) {
+                    $user = 'NoNickname';
+                    } else {
+                    $user = $getUser->fetch()['nickname'];
+                    }?>
+                
+                    <li >
+                        <a href="profile.php">
+                            <?php echo $user; ?>
+                        </a>
+                    </li>
+                    <li >
+                        <a href="app/auth/logout.php">Log out</a>
+
+                    </li>
+                </ul>
+                <?php endif; ?>
+            </div>
+        </nav>
+    </div>
+</div>
