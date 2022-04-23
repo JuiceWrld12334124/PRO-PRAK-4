0094797d78b79d4bc3f7de4bc8b5bb3e11dd1a15
JuiceWrld12334124 <vgeance50@gmail.com>
2022-04-23 23:57:33 +0200

    Delete script initial commit

---
+++ app/posts/delete.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+if (!isset($_GET['post'])) {
+    redirect('../../profile.php');
+}
+
+$removePost = $pdo->prepare('DELETE FROM posts WHERE postID = :id');
+$removePost->bindParam(':id', $_GET['post']);
+if (!$removePost->execute()) {
+    echo "Something went wrong<br><a href='../../profile.php'>Go back</a>";
+}
+$removeVotes = $pdo->prepare('DELETE FROM postVotes WHERE postID=:id');
+$removeVotes->bindParam(':id', $_GET['post']);
+$removeVotes->execute();
+redirect('../../profile.php');
