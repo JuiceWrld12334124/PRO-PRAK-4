9f606d9d1480ab06122ee17012c96107fc5d0aab
JuiceWrld12334124 <vgeance50@gmail.com>
2022-04-25 12:27:19 +0200

    Delete user script

--- app/users/delete.php
+++ app/users/delete.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
+$username = $_POST["id"];
 
-$removeUser = $pdo->prepare('DELETE FROM users WHERE nickname = :nickname');
-$removeUser->bindParam(':nickname', $_GET['nickname']);
-
+$removeUser = $pdo->prepare('DELETE FROM users WHERE id = :id');
+$removeUser->bindParam(':id', $username);
 $removeUser->execute();
 
-if ($removeUser->execute()) {
 
-    redirect('../../admin.php');    
-}
 
